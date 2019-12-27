6ae4e0b8813b9025ab85ebb58f11443312972c59
Jonathan Ray <jonathanray17@gmail.com>
2019-12-27 11:29:16 -0500

    initialization

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('Test');
+const p = document.querySelector('[data-target]');
+
+// if (location.protocol != 'https:') {
+// 	location.href =
+// 		'https:' + window.location.href.substring(window.location.protocol.length);
+// }
+function requestT() {
+	if (
+		typeof DeviceMotionEvent !== 'undefined' &&
+		typeof DeviceMotionEvent.requestPermission === 'function'
+	) {
+		// alert('enter');
+		DeviceMotionEvent.requestPermission()
+			.then(response => {
+				alert('resp' + response);
+				if (response == 'granted') {
+					window.addEventListener('devicemotion', e => {
+						// do something with e
+						p.textContent = e.acceleration.x;
+					});
+				}
+			})
+			.catch(() => alert('You denied permission'));
+	} else {
+		alert('DeviceMotionEvent is not defined');
+	}
+}
+document.getElementById('request').onclick = requestT;
