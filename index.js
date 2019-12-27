fd56ab4d679909b1f4a62068e85b1e429ae35280
Jonathan Ray <jonathanray17@gmail.com>
2019-12-27 11:45:05 -0500

    fine tuning frequency

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-console.log('Test');
+function decimalAdjust(type, value, exp) {
+	// If the exp is undefined or zero...
+	if (typeof exp === 'undefined' || +exp === 0) {
+		return Math[type](value);
+	}
+	value = +value;
+	exp = +exp;
+	// If the value is not a number or the exp is not an integer...
+	if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+		return NaN;
+	}
+	// Shift
+	value = value.toString().split('e');
+	value = Math[type](+(value[0] + 'e' + (value[1] ? +value[1] - exp : -exp)));
+	// Shift back
+	value = value.toString().split('e');
+	return +(value[0] + 'e' + (value[1] ? +value[1] + exp : exp));
+}
+
+// Decimal round
+const round10 = (value, exp) => decimalAdjust('round', value, exp);
 const p = document.querySelector('[data-target]');
 
 // if (location.protocol != 'https:') {
@@ -17,8 +37,8 @@ function requestT() {
 				if (response == 'granted') {
 					window.addEventListener('devicemotion', e => {
 						// do something with e
-						if (Math.abs(e.acceleration.x - p.textContent) > 0.1)
-							p.textContent = `${e.acceleration.x}`;
+						if (Math.abs(e.acceleration.x - parseFloat(p.textContent)) > 0.1)
+							p.textContent = `${round10(e.acceleration.x)}`;
 					});
 				}
 			})
