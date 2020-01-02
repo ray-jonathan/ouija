c94be2cf6323f2df258915d5d2d378b95af873f9
Jonathan Ray <jonathanray17@gmail.com>
2020-01-02 09:06:31 -0500

    reworks the acceleration into velocity and displacement

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function decimalAdjust(type, value, exp = -1) {
 	return +(value[0] + 'e' + (value[1] ? +value[1] + exp : exp));
 }
 
+function displace(a, t, v = 0) {
+	return v + (1 / 2) * a * t ** 2;
+}
+
 // Decimal round
 const round10 = (value, exp) => decimalAdjust('round', value, exp);
 const x = document.querySelector('[data-target=x]');
@@ -28,6 +32,8 @@ let newX = window.innerWidth / 2;
 let newY = window.innerHeight / 4;
 root.style.setProperty('--x', newX + 'px');
 root.style.setProperty('--y', newY + 'px');
+let vX = 0;
+let vY = 0;
 // if (location.protocol != 'https:') {
 // 	location.href =
 // 		'https:' + window.location.href.substring(window.location.protocol.length);
@@ -43,28 +49,27 @@ function requestT() {
 			.then(response => {
 				alert('resp' + response);
 				if (response == 'granted') {
-					window.addEventListener('devicemotion', e => {
-						// do something with e
-						// if (Math.abs(e.acceleration.x - parseFloat(x.textContent)) > 0.1) {
-						x.textContent = `${round10(e.acceleration.x)}`;
-						const xVal = parseFloat(
-							root.style.getPropertyValue('--x').replace('px', ''),
-						);
-						const stopX = (performance.now() - start) / 1000;
-						const newX = xVal + parseFloat(x.textContent) * stopX * 5;
-						root.style.setProperty('--x', newX + 'px');
-						// }
-						// if (Math.abs(e.acceleration.y - parseFloat(y.textContent)) > 0.1) {
-						y.textContent = `${round10(e.acceleration.y)}`;
-						const yVal = parseFloat(
-							root.style.getPropertyValue('--y').replace('px', ''),
-						);
-						const stopY = ((performance.now() - start) * stop) / 1000;
-						const newY = yVal + parseFloat(y.textContent) * stopY * 5;
-						root.style.setProperty('--x', newY + 'px');
-						// }
-						z.textContent = `${round10(e.acceleration.z)}`;
-					});
+					window.addEventListener(
+						'devicemotion',
+						({ acceleration: { x: rawX, y: rawY } }) => {
+							vX = round10(rawX * ((performance.now() - start) / 1000));
+							vY = round10(rawY * ((performance.now() - start) / 1000));
+							const xDisplacement = displace(
+								round10(rawX),
+								(performance.now() - start) / 1000,
+								vX,
+							);
+							const yDisplacement = displace(
+								round10(rawY),
+								(performance.now() - start) / 1000,
+								vY,
+							);
+							newX = newX + xDisplacement;
+							newY = newY + yDisplacement;
+							root.style.setProperty('--x', newX + 'px');
+							root.style.setProperty('--y', newY + 'px');
+						},
+					);
 				}
 			})
 			.catch(() => alert('You denied permission'));
@@ -73,3 +78,24 @@ function requestT() {
 	}
 }
 document.getElementById('request').onclick = requestT;
+
+// // do something with e
+// // if (Math.abs(e.acceleration.x - parseFloat(x.textContent)) > 0.1) {
+// 	x.textContent = `${round10(e.acceleration.x)}`;
+// 	const xVal = parseFloat(
+// 		root.style.getPropertyValue('--x').replace('px', ''),
+// 	);
+// 	const stopX = (performance.now() - start) / 1000;
+// 	const newX = xVal + parseFloat(x.textContent) * stopX * 5;
+// 	root.style.setProperty('--x', newX + 'px');
+// 	// }
+// 	// if (Math.abs(e.acceleration.y - parseFloat(y.textContent)) > 0.1) {
+// 	y.textContent = `${round10(e.acceleration.y)}`;
+// 	const yVal = parseFloat(
+// 		root.style.getPropertyValue('--y').replace('px', ''),
+// 	);
+// 	const stopY = ((performance.now() - start) * stop) / 1000;
+// 	const newY = yVal + parseFloat(y.textContent) * stopY * 5;
+// 	root.style.setProperty('--x', newY + 'px');
+// 	// }
+// 	z.textContent = `${round10(e.acceleration.z)}`;
